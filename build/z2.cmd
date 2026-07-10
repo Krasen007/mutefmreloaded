3909b1805dc244a21e22aa1a6839d764424e5436
Krasen Ivanov <krasen007@gmail.com>
2026-07-10 14:20:39 +0300

    Remove obsolete build artifacts

--- build/z2.cmd
+++
@@ -1,3 +0,0 @@
-@c:\pscp Output\mutefm_setup_0_9_15.exe jaredsohn_mutetunes@ssh.phx.nearlyfreespeech.net:
-
-REM @c:\pscp Output\patcher\*.* jaredsohn_mutetunes@ssh.phx.nearlyfreespeech.net:patcher
--- build/zplus2.cmd
+++
@@ -1,3 +0,0 @@
-@c:\pscp Output\mutefmplus_setup_0_9_15.exe jaredsohn_mutetunes@ssh.phx.nearlyfreespeech.net:
-
-@c:\pscp Output\patcher\*.* jaredsohn_mutetunes@ssh.phx.nearlyfreespeech.net:patcher
